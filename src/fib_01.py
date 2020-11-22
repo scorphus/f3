a0c156ed98e054640421fb33bdb2bdd47a87301a
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2020-11-22 17:06:15 +0100

    Move fib_01 to src

---
+++ src/fib_01.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of f4
+# https://github.com/scorphus/f4
+
+# Licensed under the BSD-3-Clause license:
+# https://opensource.org/licenses/BSD-3-Clause
+# Copyright (c) 2020, Pablo S. Blum de Aguiar <scorphus@gmail.com>
+
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)
+def fib(n):
+    return n if n < 2 else fib(n - 2) + fib(n - 1)
+
+
+def even_fib_sum(n):
+    i = s = 0
+    while True:
+        f = fib(i)
+        if f > n:
+            return s
+        if f % 2 == 0:
+            s += f
+        i += 1
