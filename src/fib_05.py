399cc2091dd65d7e31b89066c0af12db87039e92
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2020-11-23 00:30:13 +0100

    🐍 Add fib_05 that calc the sum iteratively, 3 by 3, at once

---
+++ src/fib_05.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of f4
+# https://github.com/scorphus/f4
+
+# Licensed under the BSD-3-Clause license:
+# https://opensource.org/licenses/BSD-3-Clause
+# Copyright (c) 2020, Pablo S. Blum de Aguiar <scorphus@gmail.com>
+
+
+def setup():
+    pass
+
+
+def header():
+    return ""
+
+
+# ---8<---
+def even_fib_sum(n):
+    a1, a2, a3 = 1, 1, 2
+    while a3 < n:
+        a1 = a2 + a3
+        a2 = a1 + a3
+        a3 = a1 + a2
+    return (a2 - 1) // 2
